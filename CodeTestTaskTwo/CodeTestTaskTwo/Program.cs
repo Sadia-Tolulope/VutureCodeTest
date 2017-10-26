88234f5ed3cf5e1e7c58ed7587d725d836d692f8
Sadia-Tolulope <tolulopeabass@aol.com>
2017-10-26 07:10:28 +0100

    Add files via upload

---
+++ CodeTestTaskTwo/CodeTestTaskTwo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTestTaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "I have some cheese";
+            Console.WriteLine(input1);
+            Console.WriteLine(IsPalindrome(input1));
+            Console.WriteLine();
+            string input2 = "God saved Eva’s dog";
+            Console.WriteLine(input2);
+            Console.WriteLine(IsPalindrome(input2));
+
+            //Console.WriteLine("Enter a String");
+            //input = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+
+            int min = 0;
+            int max = input.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char text = input[min];
+                char word = input[max];
+
+                while (!char.IsLetterOrDigit(text))
+                {
+                    min++;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    text = input[min];
+                }
+
+                // Scan backward for word while invalid.
+                while (!char.IsLetterOrDigit(word))
+                {
+                    max--;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    word = input[max];
+                }
+
+                if (char.ToLower(text) != char.ToLower(word))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+    }
+}
---
+++ CodeTestTaskTwo/CodeTestTaskTwoTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeTestTaskTwoTest
+{
+    [TestClass]
+    public class CodeTestTaskTwo
+    {
+        [TestMethod]
+        public void TesttTaskTwo()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
