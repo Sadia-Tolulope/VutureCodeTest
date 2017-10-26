027cb0191672f8e1ffafed6c5a0abae9a0d51026
Sadia-Tolulope <tolulopeabass@aol.com>
2017-10-26 07:09:19 +0100

    Add files via upload

---
+++ CodeTestTaskOne/CodeTestTaskOne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTestTaskOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "I have some cheese";
+            int count = 0;
+
+            foreach (char item in input)
+            {
+                if (item == 'e')
+                {
+                    count++;
+                }
+            }
+
+            //Console.WriteLine(input);
+            //The number of time the character E occurrences is :
+            Console.WriteLine(" {0}", count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeTestTaskOne/CodeTestTaskOneTest/CodeTestTaskOneTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeTestTaskOneTest
+{
+    [TestClass]
+    public class TaskOneTest
+    {
+        [TestMethod]
+        public void TestTaskOne()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
