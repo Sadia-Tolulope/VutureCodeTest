708ddaa353e64781452663c6f991662ed29ab9f9
Sadia-Tolulope <tolulopeabass@aol.com>
2017-10-26 07:14:42 +0100

    Add files via upload

---
+++ CodeTaskThreeC/CodeTaskThreeC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTaskThreeC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Anna went to vote in the election to fulfil her civic duty";
+
+            Console.WriteLine("Input");
+            Console.WriteLine(input);
+            Console.WriteLine();
+            input = input.Replace("Anna", "A&&a")
+                            .Replace("civic", "c$$$c");
+            Console.WriteLine("Output");
+            Console.WriteLine(input);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CodeTaskThreeC/CodeTestTaskThreeCTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeTestTaskThreeCTest
+{
+    [TestClass]
+    public class TaskThreeC
+    {
+        [TestMethod]
+        public void TestThreeC()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
