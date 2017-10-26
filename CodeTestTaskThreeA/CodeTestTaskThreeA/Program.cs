effc18955a3ad84f331843d09519cdddbfa50bd7
Sadia-Tolulope <tolulopeabass@aol.com>
2017-10-26 07:11:55 +0100

    Add files via upload

---
+++ CodeTestTaskThreeA/CodeTestTaskThreeA/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTestTaskThreeA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] censoredwords = { "cat", "dog", "large" };
+            string input = "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.";
+            var total = 0;
+            Console.WriteLine();
+            Console.WriteLine("Input");
+            Console.WriteLine(input);
+            Console.WriteLine();
+            Console.WriteLine("Output");
+            foreach (string item in censoredwords)
+            {
+
+                if (input.Contains(item))
+                {
+
+                    Console.Write("{0}: {1},  ", item, TextCount.CountOccurrences(input, item));
+                    total += TextCount.CountOccurrences(input, item);
+                }
+            }
+
+            Console.WriteLine("Total: {0}", total);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CodeTestTaskThreeA/CodeTestTaskThreeA/TextCount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTestTaskThreeA
+{
+    public static class TextCount
+    {
+        public static int CountOccurrences(string text, string pattern)
+        {
+            int count = 0;
+            int i = 0;
+            //int total = 0;
+            while ((i = text.IndexOf(pattern, i)) != -1)
+            {
+                i += pattern.Length;
+                count++;
+                //total += i;
+            }
+
+            return count;
+
+        }
+    }
+}
