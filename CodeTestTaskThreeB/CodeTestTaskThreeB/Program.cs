2360df4cb3501a53fce8f683379732eab3220f2d
Sadia-Tolulope <tolulopeabass@aol.com>
2017-10-26 07:13:14 +0100

    Add files via upload

---
+++ CodeTestTaskThreeB/CodeTestTaskThreeB/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeTestTaskThreeB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse!";
+            string[] censoredword = { "meow", "woof" };
+            string censoredText = "$$$$";
+            string pattern = @"(eo|oo)";
+
+            IEnumerable<Regex> censoredWordMatchers = censoredText.
+                         Select(x => new Regex(string.Format(pattern.ToLower(), x)));
+
+            string output = censoredWordMatchers.
+                    Aggregate(input, (current, matcher) => matcher.Replace(current, censoredText));
+
+            Console.WriteLine("Input");
+            Console.WriteLine(input);
+            Console.WriteLine();
+            Console.WriteLine("Output");
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CodeTestTaskThreeB/CodeTestTaskThreeBTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeTestTaskThreeBTest
+{
+    [TestClass]
+    public class TaskThreeB
+    {
+        [TestMethod]
+        public void TestThreeB()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
